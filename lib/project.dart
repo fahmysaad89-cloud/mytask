93d5dc1fbb3dcdc72f91df029017fb998b2d308e
fahmysaad89-cloud <fahmysaad89@gmail.com>
2026-07-21 00:33:57 +0200

    print day

--- lib/project.dart
+++ lib/project.dart
@@ -28,4 +28,27 @@ void main() {
   }).toList();
 
   print(even);
+
+  // Q4
+
+  int day = 11;
+
+  switch (day) {
+    case 1:
+      print("Monday");
+    case 2:
+      print("Tuesday");
+    case 3:
+      print("Wednesday");
+    case 4:
+      print("Thursday");
+    case 5:
+      print("Friday");
+    case 6:
+      print("Saturday");
+    case 7:
+      print("Sunday");
+    default:
+      print("Invalid day");
+  }
 }

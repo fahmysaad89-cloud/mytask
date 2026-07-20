5bb67cec5c49ac9205c0ab09b31dff915ca02967
fahmysaad89-cloud <fahmysaad89@gmail.com>
2026-07-20 22:49:28 +0200

    addfavFood

---
+++ lib/project.dart
@@ -0,0 +1,20 @@
+void main() {
+  //Q1:
+
+  for (int i = 1; i <= 20; i++) {
+    print(i);
+  }
+
+  // Q2:
+  List<String> favFood = [
+    "Pasta",
+    "Pizza",
+    "Burger",
+    "Breaded chicken",
+    "Chicken",
+  ];
+
+  for (int i = 0; i < favFood.length; i++) {
+    print(favFood[i]);
+  }
+}

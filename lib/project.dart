0a0769c8ba15c244d3fdf20637fdf704172b783e
fahmysaad89-cloud <fahmysaad89@gmail.com>
2026-07-24 16:08:54 +0200

    fruits

--- lib/project.dart
+++ lib/project.dart
@@ -9,4 +9,28 @@ void main() {
       print(i);
     }
   }
+
+  // Q2:
+  List<String> fruits = ['apple', 'banana', 'mango', 'kiwi'];
+  for (int i = 0; i < fruits.length; i++) {
+    print("$i:${fruits[i]}");
+  }
+
+  // Q3:
+  List<int> scores = [55, 82, 90, 43, 77];
+  int sum = 0;
+  for (int i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  }
+
+  double avg = sum / scores.length;
+
+  print("sum : $sum");
+  print("avg : $avg");
+
+  // Q4:
+  Map<String, int> ages = {'Sara': 20, 'Omar': 23, 'Laila': 19};
+  ages.forEach((name, age) {
+    print("$name is $age years old");
+  });
 }
